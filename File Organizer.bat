69bf435c4d687b292403ac5afd9bb5f826abb2c4
Kikkiu17 <71791193+Kikkiu17@users.noreply.github.com>
2021-08-20 13:59:06 +0200

    Update File Organizer.bat

--- File Organizer.bat
+++ File Organizer.bat
@@ -1,3 +1,13 @@
 @echo off
-for /f "delims=" %%i in ('dir /b /a-d /s .\organizer-main.py') do python %%i
+setlocal EnableDelayedExpansion
+
+echo Cerco il file dello script...
+for /f "delims=" %%a in ('dir /s /b organizer-main.py') do set "string=%%a"
+
+rem Do the split:
+set i=1
+set "fn!i!=%string:organizer-main.py=" & set /A i+=1 & set "fn!i!=%"
+set fn
+cd %fn1%
+python organizer-main.py
 pause
